0d94c0c530ccd3ae95bc5cc6b26881e410096423
Mohammad AbuSafat <mo7ammad.abusafat@gmail.com>
2022-11-26 11:32:27 +0200

    s

--- src/View/QPage/index.js
+++ src/View/QPage/index.js
@@ -3,14 +3,7 @@ import AddPost from "../../Components/AddPost";
 import styles from "./styles.module.css";
 
 const QPage = () => {
-<<<<<<< HEAD
   return <div className={styles.MainWrapper}></div>;
-=======
-  return (
-    <></>
-  );
-
->>>>>>> c8bc22f8b6872ed2b70c5e21a84fcbb6697e960f
 };
 
 export default QPage;
