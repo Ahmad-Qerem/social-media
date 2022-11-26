07980063e1fc40fd8eb0685e8180ede5ef9fabb3
Ahmad-Qerem <alqerema@gmail.com>
2022-11-26 11:10:02 +0200

    test

--- src/View/HomePage/index.js
+++ src/View/HomePage/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import AddPost from "../../Components/AddPost";
 import styles from './styles.module.css'
 
 const HomePage = () => {
   return (
     <div className={styles.MainWrapper}>
-        HomePage
+        <AddPost/>
     </div>
     );
 };
