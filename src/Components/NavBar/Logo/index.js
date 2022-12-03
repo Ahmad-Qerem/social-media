b6afd9c356b1e304cf39c709050258d58c211c08
Hala <83414449+Hala-Salah@users.noreply.github.com>
2022-12-03 14:25:20 +0200

    QPage

--- src/Components/NavBar/Logo/index.js
+++ src/Components/NavBar/Logo/index.js
@@ -3,7 +3,7 @@ import styles from './styles.module.css'
 const Logo = () => {
   return (
     <div className={styles.Logo}>
-          <img src='' alt='Logo'/>
+          <img src='/assets/images/Overflow-logo.png' alt='Logo'/>
     </div>
   )
 }
