b2a13e9ddb4cddf48ea9ae7717e1464b4c8f0b1b
Ahmad-Qerem <alqerema@gmail.com>
2022-12-03 14:40:30 +0200

    nav bar

--- src/Components/NavBar/ListItems/index.js
+++ src/Components/NavBar/ListItems/index.js
@@ -4,19 +4,49 @@ import { Link } from "react-router-dom";
 import Search from "../Search";
 import styles from "./styles.module.css";
 const ListItems = () => {
-  const ListTitles = ["Home", "About"];
-  const UserRouts = ["Log In", "Sign Up"];
+  const ListTitles = [
+    {
+      Title: "Home",
+      Link: "/",
+    },
+    {
+      Title: "About",
+      Link: "About",
+    },
+  ];
+
+  const UserRouts = [
+    {
+      Title: "Log In",
+      Link: "/Login",
+    },
+    {
+      Title: "Sign Up",
+      Link: "/Signup",
+    },
+  ];
   return (
     <div className={styles.MainWrapper}>
       <div className={styles.Items}>
         {ListTitles.map((item) => (
           <ListItem className={styles.ListItem}>
-            <Link>{item}</Link>
+            <Link className={styles.LinkTitle} to={item.Link}>
+              {item.Title}
+            </Link>
           </ListItem>
         ))}
       </div>
 
       <Search />
+      <div className={styles.UserState}>
+        {UserRouts.map((item) => (
+          <ListItem className={styles.ListItem}>
+            <Link to={item.Link} className={styles.LinkTitle}>
+              {item.Title}
+            </Link>
+          </ListItem>
+        ))}
+      </div>
     </div>
   );
 };
--- src/Components/NavBar/Search/index.js
+++ src/Components/NavBar/Search/index.js
@@ -1,15 +1,21 @@
-import { Input, TextField } from '@mui/material'
-import React from 'react'
-import styles from './styles.module.css'
+import { Input, TextField } from "@mui/material";
+import React from "react";
+import styles from "./styles.module.css";
 
 const Search = () => {
   return (
-    <div className={styles.MainWrapper}>
-        <TextField id="outlined-search" label="Search field" type="search" className={styles.Search}/>
+    <>
+      <div className={styles.MainWrapper}>
+        <TextField
+          id="outlined-search"
+          label="Search field"
+          type="search"
+          className={styles.Search}
+        />
+      </div>
+      <img src="./images/Search.png" className={styles.SearchIcon} />
+    </>
+  );
+};
 
-        
-    </div>
-  )
-}
-
-export default Search
+export default Search;
